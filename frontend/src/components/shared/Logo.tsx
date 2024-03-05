ff8cd46b71f564cbc17f38b6c43fad937acd5559
richmond-andoh <richmondandoh00@gmail.com>
2024-03-05 10:45:19 +0000

    public folder and src folder modified

--- frontend/src/components/shared/Logo.tsx
+++ frontend/src/components/shared/Logo.tsx
@@ -1,9 +1,15 @@
 import { Link } from "react-router-dom"
+import { Typography } from "@mui/material"
 
 const Logo = () => {
   return (
-    <div>
-      
+    <div style={{ display: "flex", alignItems: "center", marginRight: "auto", gap: "16px"}}>
+      <Link to={"/"}>
+        <img src="openai.png" alt="openai logo" width={"35px"} height={"35px"}/>
+      </Link>
+      <Typography  variant="h4">
+
+      </Typography>
     </div>
   )
 }
