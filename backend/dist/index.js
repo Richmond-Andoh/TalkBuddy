102d74bf628e55a8dc01be19d7fc9387633099e6
richmond-andoh <richmondandoh00@gmail.com>
2024-03-08 21:41:24 +0000

    login functionality implemented with axios package

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -4,11 +4,13 @@ import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import dbconnection from "./db/dbconnection.js";
 import appRoute from "./routes/indexRoute.js";
+import cors from "cors";
 const app = express();
 dbconnection();
 const PORT = process.env.PORT || 8000;
 dotenv.config();
 // Middlewares
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 // Remove from production
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import morgan from "morgan"
 import cookieParser from "cookie-parser";
 import dbconnection from "./db/dbconnection.js";
 import appRoute from "./routes/indexRoute.js";
+import cors from "cors"
 
 const app = express();
 
@@ -13,6 +14,7 @@ const PORT = process.env.PORT || 8000
 dotenv.config();
 
 // Middlewares
+app.use(cors({ origin: "http://localhost:5173", credentials: true}))
 app.use(express.json())
 app.use(cookieParser(process.env.COOKIE_SECRET))
 
